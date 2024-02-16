2eb79befe15321905a5b1d86593af5e2ab49d175
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-02-16 16:22:18 -0500

    refactor: Ignore Unknown language in repos

--- stats_calculator.py
+++ stats_calculator.py
@@ -1,15 +1,23 @@
 def calculate_language_stats(repos: list) -> dict:
     langs_stats = {}
 
+    unknown_count = 0
+
     for repo in repos:
         language = repo['language'] or 'Unknown'
+
+        if language == 'Unknown':
+            unknown_count += 1
+            continue
+
         if language in langs_stats:
             langs_stats[language]["count"] += 1
         else:
             langs_stats[language] = {"count": 1, "percentage": 0}
 
-    total_repos = len(repos)
+    total_valid_repos = len(repos) - unknown_count
+
     for lang in langs_stats:
-        langs_stats[lang]['percentage'] = round((langs_stats[lang]['count'] / total_repos) * 100, 2) if total_repos > 0 else 0
+        langs_stats[lang]['percentage'] = round((langs_stats[lang]['count'] / total_valid_repos) * 100, 2) if total_valid_repos > 0 else 0
     
     return langs_stats
