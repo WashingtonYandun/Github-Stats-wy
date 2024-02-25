4e031cd028f562618cae52a3a5846da49915de91
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-02-24 21:53:56 -0500

    refactor: Add commentaries

--- app/app.py
+++ app/app.py
@@ -5,6 +5,19 @@ app = Flask(__name__)
 
 @app.route('/langs/<username>/<chart>', methods=['GET'])
 def langs(username: str, chart: str) -> Response:
+    """
+    Retrieve the language statistics for a given user and generate an SVG chart.
+
+    Args:
+        username (str): The username of the GitHub user.
+        chart (str): The type of chart to generate. Valid options are 'bar', 'pie', and 'donut'.
+
+    Returns:
+        Response: The SVG chart image as a Flask Response object.
+
+    Raises:
+        Exception: If an error occurs during the process.
+    """
     try:
         svg_image = lang_service(username, chart, request.args)
         return Response(svg_image, mimetype='image/svg+xml')
